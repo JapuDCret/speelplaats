5affad28583fb47d8f63fccfa5c40db5531c8c1d
JapuDCret <JapuDCret@users.noreply.github.com>
2023-10-13 21:31:35 +0200

    feat(apps): add spring-test

---
+++ apps/spring-test/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.4"
+	id("io.spring.dependency-management") version "1.1.3"
+    id("com.google.cloud.tools.jib") version "3.3.1"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+}
+
+group = "de.chasermaser"
+version = "0.0.1"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+jib {
+	from {
+		image = "gcr.io/distroless/java17-debian12:latest"
+	}
+	to {
+		image = "JapuDCret/speelplaats"
+	}
+}
---
+++ apps/spring-test/src/main/kotlin/de/chasermaser/springtest/SpringTestApplication.kt
@@ -0,0 +1,11 @@
+package de.chasermaser.springtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringTestApplication>(*args)
+}
