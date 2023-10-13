abd0b7f39aa0b01fce071bc121289d10814bc254
JapuDCret <JapuDCret@users.noreply.github.com>
2023-10-13 22:43:15 +0200

    fix(cicd): fix image name^2

--- apps/spring-test/build.gradle.kts
+++ apps/spring-test/build.gradle.kts
@@ -46,6 +46,6 @@ jib {
 		image = "gcr.io/distroless/java17-debian12:latest"
 	}
 	to {
-		image = "${System.getenv("IMAGE_NAME")}" ?: "JapuDCret/speelplaats"
+		image = "${System.getenv("IMAGE_NAME")}" ?: "japudcret/speelplaats"
 	}
 }
